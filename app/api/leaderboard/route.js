b214efcf52b8497ed24d8ae183f9163f06cd00da
mattbarbour22-rgb <mattbarbour22@gmail.com>
2026-06-01 17:40:12 +1200

    Update route.js

--- app/api/leaderboard/route.js
+++ app/api/leaderboard/route.js
@@ -1,4 +1,4 @@
-export const revalidate = 300000;
+export const revalidate = 999999999999999;
 
 const TOURN_ID = process.env.SLASH_GOLF_TOURN_ID || '033';
 const YEAR = process.env.SLASH_GOLF_YEAR || '2026';
