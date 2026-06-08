3525f8cf7dc2ae8b066faf41a3973f4927aa63a5
mattbarbour22-rgb <mattbarbour22@gmail.com>
2026-06-08 21:20:20 +1200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -682,7 +682,7 @@ const eliminatedCount = pool.filter(p => p.eliminated).length;
         </div>
       </div>
 
-      <div className="note">Live rankings are decided by best current golf position, then next best and third pick. {tournamentConfig.jackpotRule}</div>
+      <div className="note">Live rankings are decided by best current golf position, then next best and third pick. To win, your picks must include the outright PGA Championship winner. Final results are preliminary and must be verified by Jonesy.</div>
     </main>
   );
 }
