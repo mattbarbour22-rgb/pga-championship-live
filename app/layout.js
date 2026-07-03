ef8e1907efa3953238548c38b58905fa9c297d7c
mattbarbour22-rgb <mattbarbour22@gmail.com>
2026-07-04 00:21:13 +1200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'U.S. Open Pick 3 Live',
-  description: 'Live Pick 3 golf pool leaderboard for the 126th U.S. Open.'
+  title: 'PGA Chamionship Pick 3 Live',
+  description: 'Live Pick 3 golf pool leaderboard for the PGA Championship.'
 };
 
 export default function RootLayout({ children }) {
